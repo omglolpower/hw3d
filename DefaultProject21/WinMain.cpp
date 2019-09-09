0ee82a923c6b8106ef41e2475627b5fad4668270
Бондарь Павел <bondar-p@yandex.ru>
2019-09-10 00:24:25 +0300

    1

--- DefaultProject21/WinMain.cpp
+++ DefaultProject21/WinMain.cpp
@@ -1,6 +1,9 @@
 
 #include <Windows.h>
 #include "WindowsMessageMap.h"
+#include <iostream>
+#include <string>
+#include <sstream>
 // define custom windows procedure:
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -24,6 +27,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			SetWindowText(hWnd, "F up");
 		}
 		break;
+	case WM_CHAR:
+	{
+		static std::string title;
+		title.push_back((char)wParam);
+		SetWindowText(hWnd, title.c_str());
+	}
+		
+		break;
+
+	case WM_LBUTTONDOWN:
+	{
+		POINTS pt = MAKEPOINTS(lParam);
+		std::ostringstream oss;
+		oss << "(" << pt.x << "," << pt.y << ")";
+		SetWindowText(hWnd, oss.str().c_str());
+	}
+	break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
---
+++ DefaultProject21/src/disable_W.h_nonsense.h
@@ -0,0 +1,40 @@
+#pragma once
+#define _WIN32WINNT 0x0601
+#include <sdkddkver.h>
+
+#define WIN32_LEAN_AND_MEAN
+#define NODDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILTER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE 
+#define NOTEAP
+
+#define STRICT
+
+#include <Windows.h>
